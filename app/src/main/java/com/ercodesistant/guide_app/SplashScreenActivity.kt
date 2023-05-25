c37773aa6b1ac0082cdbc9acaa6f7283f45f37c9
nadhilamizayaasriel01 <91447672+nadhilamizayaasriel01@users.noreply.github.com>
2023-05-25 16:21:35 +0700

    Splash Screen

---
+++ app/src/main/java/com/ercodesistant/guide_app/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package com.ercodesistant.guide_app
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 1000 // 1 detik
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Mendeteksi mode malam
+        val nightModeFlags = resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+        val isNightMode = nightModeFlags == Configuration.UI_MODE_NIGHT_YES
+
+        // Menampilkan layout splash screen yang sesuai dengan mode
+        if (isNightMode) {
+            setContentView(R.layout.activity_splash_screen_night)
+        } else {
+            setContentView(R.layout.activity_splash_screen)
+        }
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
+
+    }
+}
