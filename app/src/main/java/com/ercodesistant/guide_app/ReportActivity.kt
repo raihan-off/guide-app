c02478bf4886306d6e65a79a245e8b2336c031c6
nadhilamizayaasriel01 <91447672+nadhilamizayaasriel01@users.noreply.github.com>
2023-06-09 08:33:09 +0700

    edit profile, profile, dan report

--- app/src/main/java/com/ercodesistant/guide_app/ReportActivity.kt
+++ app/src/main/java/com/ercodesistant/guide_app/ReportActivity.kt
@@ -1,13 +1,45 @@
 package com.ercodesistant.guide_app
 
+import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.ercodesistant.guide_app.databinding.ActivityReportBinding
 
 class ReportActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        // Customize the activity as needed
-    }
+
+    //private val binding: ActivityReportBinding = ActivityReportBinding.inflate(layoutInflater)
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_report)
+//
+//
+//        setContentView(binding.root)
+//
+////        val spinnerKategori: Spinner = binding.spinnerKategori
+////
+////        val adapter = ArrayAdapter.createFromResource(this, R.array.kategori_options, android.R.layout.simple_spinner_item)
+////        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+////        spinnerKategori.adapter = adapter
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//    }
+
+//    private var _binding = ActivityReportBinding? = null
+//            private val binding get() = _binding!!
+//
+//    override fun onCreateView(inflater: LayoutInflater,
+//                              container: ViewGroup?,
+//                              savedInstanceState: Bundle?): View? {
+//        _binding = ActivityReportBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//    }
 }
